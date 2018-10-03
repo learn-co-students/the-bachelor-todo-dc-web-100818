e366b1aa2bb8a26a3185dd8f674026aff018b348
Ana Harris <vana1990@gmail.com>
2018-10-03 04:11:01 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,22 @@
+require "pry"
+#require 'json'
+#data = JSON.parse(File.read('../spec/fixtures/contestants.json'))
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winners_name = ""
+  winner = nil
+  data[season].each do |arr_el|
+    # puts arr_el.inspect
+    if arr_el["status"] == "Winner"
+      winners_name = arr_el["name"]
+      winner = winners_name.split(" ").first
+    end
+  end
+  return winner
 end
+  
+
+#get_first_name_of_season_winner(data, "season 14")
 
 def get_contestant_name(data, occupation)
   # code here
